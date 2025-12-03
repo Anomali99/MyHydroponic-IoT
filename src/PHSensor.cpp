0699f3020cd2913abda975983110f984d2917e20
Anomali99 <fatiqnur@gmail.com>
2025-12-03 23:09:10 +0700

    add tds calibration

--- src/PHSensor.cpp
+++ src/PHSensor.cpp
@@ -5,7 +5,6 @@ PHSensor::PHSensor(Adafruit_ADS1115 &ads, byte pin) : _ads(ads), _pin(pin) {}
 float PHSensor::readPh()
 {
     int16_t adcValue = _ads.readADC_SingleEnded(_pin);
-
     float voltage = _ads.computeVolts(adcValue);
     float phValue = 7.0 + ((_phOffset - voltage) / _phSlope);
 
--- src/TDSSensor.cpp
+++ src/TDSSensor.cpp
@@ -5,11 +5,11 @@ TDSSensor::TDSSensor(Adafruit_ADS1115 &ads, byte pin) : _ads(ads), _pin(pin) {}
 float TDSSensor::readTDS(float temp)
 {
     int16_t adcValue = _ads.readADC_SingleEnded(_pin);
-
     float voltage = _ads.computeVolts(adcValue);
-    float compensationCoefficient = 1.0 + 0.02 * (temp - 25.0);
-    float compensatedVoltage = voltage / compensationCoefficient;
-    float tdsValue = (compensatedVoltage * 130.0) * _kValue;
+
+    float ecValue = (133.42 * voltage * voltage * voltage - 255.86 * voltage * voltage + 857.39 * voltage) * kValue;
+    float ecValue25 = ecValue / (1.0 + 0.02 * (temp - 25.0));
+    float tdsValue = ecValue25 * tdsFactor;
 
     return tdsValue;
 }
